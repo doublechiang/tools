d48f5102cdb30cf0d31b371469577e62b20a57c3
Jiang Junyu <double.chiang@gmail.com>
2013-05-29 16:58:19 +0800

    Add a install script and git configuration file.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+CONFIGURATIONS="emacs gitconfig vimrc"
+dir=`pwd`
+
+for confile in $CONFIGURATIONS; do
+   echo "installing $confile from this package..."
+
+   # if previous file exist
+   # prompt to erase it or not
+   #    if not to remove it, then skip this confiuration file.
+   # fi
+   # create the symbolic link
+   if [ -e ~/.${confile} ]; then
+
+       echo "previous configuration file ${confile} exist"
+       read -p "do you want to remove it, otherwise it will not be installed (y/n) " answer
+       if [ $answer == "y" ]; then
+	   rm ~/.${confile}
+       else
+	   continue
+       fi
+   fi
+   ln -s ${dir}/$confile ~/.${confile}
+done
