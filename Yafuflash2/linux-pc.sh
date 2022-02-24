23cd618d8d6ae2093b72bd4f00a553d765e5690e
Jiangjunyu <double.chiang@gmail.com>
2022-02-24 11:32:40 -0800

    enhance the script can be run anywhere.

--- Yafuflash2/linux-pc.sh
+++ Yafuflash2/linux-pc.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-cd linuxflash
-./linuxflash-pc.sh
-cd ..
+echo ""
+echo BMC firmware image: $filename
+
+filename=$1
+
+pd=`dirname $0`
+plat=`uname -m | cut -c 1-6`
+arch='Linux_x86_32'
+if [ "$plat" = "x86_64" ]; then
+	arch='Linux_x86_64'
+fi
+
+$pd/linuxflash/$arch/Yafuflash2 -vyes -cd -fb -pipmi -pauth -pnet -psnmp -pssh -pkvm -pntp -psel $filename
+
